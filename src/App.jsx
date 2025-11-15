7ed47d852cb704d0da8e742d2a3113b84b564b69
Nyla Alston <114892714+nylajade29@users.noreply.github.com>
2025-11-15 09:48:31 -0500

    Create App component with Dashboard integration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+
+export default function App() {
+  return (
+    <main>
+      <h1>MindCast</h1>
+      <Dashboard />
+    </main>
+  );
+}
